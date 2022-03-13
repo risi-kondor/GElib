bb0f34e69abd2f35ba844879ea891fcc0be381c6
Risi <risi@cs.uchicago.edu>
2022-03-12 21:25:59 -0600

    'changes'

--- objects/SO3/testB/timeSO3CG.cpp
+++ objects/SO3/testB/timeSO3CG.cpp
@@ -13,12 +13,18 @@ int main(int argc, char** argv){
   GElibSession session;
   cout<<endl;
 
-  int b=1;
+  int b=10;
   int l1=2;
   int l2=2;
   int l=3;
-  int n1=2;
-  int n2=2;
+  int n1=20;
+  int n2=20;
+
+#ifdef _WITH_CUDA
+  SO3partB u0=SO3partB::gaussian(1,l1,1,1);
+  SO3partB v0=SO3partB::gaussian(1,l2,1,1);
+  SO3partB w0=u0.CGproduct(v0,l);
+#endif
 
   SO3partB u=SO3partB::gaussian(b,l1,n1);
   SO3partB v=SO3partB::gaussian(b,l2,n2);
@@ -27,15 +33,19 @@ int main(int argc, char** argv){
   printl("u",u)<<endl;
   printl("v",v)<<endl;
 
+  cout<<"Starting CPU"<<endl;
   SO3partB w=u.CGproduct(v,l);
-  cout<<w<<endl;
+  //cout<<w<<endl;
+  cout<<"."<<endl;
 
 #ifdef _WITH_CUDA
   SO3partB ug=u.to_device(1);
   SO3partB vg=v.to_device(1);
+  cout<<"Starting CPU"<<endl;
   SO3partB wg=ug.CGproduct(vg,l);
 
-  cout<<wg<<endl;
+  //cout<<wg<<endl;
+  cout<<"."<<endl;
 #endif 
 
   cout<<endl; 
