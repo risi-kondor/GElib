39f1aceb8925d6d7a0e564f1865f06570ad7c886
Risi Kondor <risi@cs.uchicago.edu>
2022-03-27 13:51:33 -0500

    'changes'

--- python/setup.py
+++ python/setup.py
@@ -10,7 +10,7 @@ import time
 
 # --- User settings ------------------------------------------------------------------------------------------
 
-compile_with_cuda=True
+compile_with_cuda=False 
 
 copy_warnings=True
 torch_convert_warnings=True 
