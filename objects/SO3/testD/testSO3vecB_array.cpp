1edd8d86d8772596f685d2f851c7e67c9e66d85d
Risi <risi@cs.uchicago.edu>
2023-01-19 15:19:51 -0600

    'changes'

--- objects/SO3/testD/testSO3vecB_array.cpp
+++ objects/SO3/testD/testSO3vecB_array.cpp
@@ -68,5 +68,39 @@ int main(int argc, char** argv){
   printl("vgc",vgc);
 #endif 
 
+
+  if(true){
+
+    cout<<"----------- DiagCGproduct -----------------------"<<endl;
+
+    
+    SO3vecB_array w=u.DiagCGproduct(v,2);
+    cout<<w<<endl;
+    
+    cout<<endl; 
+    
+    #ifdef _WITH_CUDA
+    SO3vecB_array uc=u.to_device(1);
+    SO3vecB_array vc=v.to_device(1);
+    
+    //printl("uc",uc)<<endl;
+    //printl("vc",vc)<<endl;
+    
+    SO3vecB_array wc=uc.DiagCGproduct(vc,2);
+    cout<<wc<<endl;
+    #endif 
+    
+    SO3vecB_array ug=SO3vecB_array::zeros_like(u);
+    SO3vecB_array vg=SO3vecB_array::zeros_like(v);
+    SO3vecB_array wg=SO3vecB_array::gaussian_like(w);
+    
+    #ifdef _WITH_CUDA
+    SO3vecB_array ugc=ug.to_device(1);
+    SO3vecB_array vgc=vg.to_device(1);
+    SO3vecB_array wgc=wg.to_device(1);
+    #endif
+
+  }
+
 }
 
