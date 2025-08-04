5815b3bf125f30573ef1f8c59ef51f0614017038
Risi Kondor <risi.kondor@gmail.com>
2025-08-04 15:53:05 -0500

    'changes'

--- benchmarking/benchmark_CGproduct.cpp
+++ benchmarking/benchmark_CGproduct.cpp
@@ -4,7 +4,9 @@
 #include "SO3part.hpp"
 #include "CGproduct.hpp"
 #include "DiagCGproduct.hpp"
-#include "LatexDocument.hpp"
+#include "LatexDoc.hpp"
+#include "LatexTable.hpp"
+#include "TikzPlot.hpp"
 
 
 using namespace cnine;
@@ -35,17 +37,24 @@ int count_triples(int l1, int l2, int l){
 }
 
 
-int main(int argc, char** argv){
-  int dev=0;
-  if(argc>1) dev=stoi(argv[1]);
 
-  //int niter=10;
-  vector<int> l_values({7,12,15});
-  vector<int> nc_values({4,8,16,32,64});
-  vector<int> b_values({1,8,32});
+TensorView<double> benchmark(vector<int> b_values, vector<int> l_values, vector<int> nc_values, const int dev){
+  TensorView<double> R({b_values.size(),l_values.size(),l_values.size(),l_values.size(),nc_values.size()},0,0);
 
-  TensorView<double> flops1({b_values.size(),l_values.size(),l_values.size(),l_values.size(),nc_values.size()},0,0);
+  int ntot=0;
+  for(int _l1=0; _l1<l_values.size(); _l1++){
+    int l1=l_values[_l1];
+    for(int _l2=0; _l2<l_values.size(); _l2++){
+      int l2=l_values[_l2];
+      for(int _l=0; _l<l_values.size(); _l++){
+	int l=l_values[_l];
+	if(l>=abs(l1-l2) && l<=l1+l2) ntot++;
+      }
+    }
+  }
+  ntot*=b_values.size()*nc_values.size();
 
+  int c=0;
   for(int _b=0; _b<b_values.size(); _b++){
     int b=b_values[_b];
     for(int _l1=0; _l1<l_values.size(); _l1++){
@@ -58,8 +67,9 @@ int main(int argc, char** argv){
 	  for(int _nc=0; _nc<nc_values.size(); _nc++){
 	    int nc=nc_values[_nc];
 
-	    size_t n_ops=((size_t)4)*count_triples(l1,l2,l)*nc*nc*b*b;
+	    size_t n_ops=((size_t)4)*count_triples(l1,l2,l)*nc*nc*b;
 	    int niter=pow(10,min(max(1,6-(int)log10(n_ops)),5));
+	    cout<<c<<"/"<<ntot<<" ";
 	    cout<<"Running "<<niter<<" CG-products with ";
 	    cout<<"b="<<b<<" ";
 	    cout<<"l1="<<l1<<" ";
@@ -77,23 +87,60 @@ int main(int argc, char** argv){
 	    auto time=timer.time();
 	    double speed=n_ops*niter/time;
 	    
-	    flops1.set(_b,_l1,_l2,_l,_nc,speed);
+	    R.set(_b,_l1,_l2,_l,_nc,speed);
 	    cout<<"Time: "<<time<<" ms; ";
 	    cout<<"Speed: "<<speed/1000<<" Mflops ";
 	    cout<<endl;
-	    
+	    c++;
+
 	  }
 	}
 	
       }
     }
 
-    LatexDoc doc;
-
-    doc<<"\section{CG-product}"<<endl;
-
-    doc.compile("results");
-
   }
+
+  return R;
+}
+
+int main(int argc, char** argv){
+  int dev=0;
+  if(argc>1) dev=stoi(argv[1]);
+
+  vector<int> l_val({2,3,5,7,10,12});
+  vector<int> nc_val({4,8,16,32,64});
+  vector<int> b_val({1,8,32});
+
+  auto speed=benchmark(b_val,l_val,nc_val,dev);
+  TensorView<int> flops0=speed.map<int>([](const Gindex& dummy, const double& x){return (int)(x/1000);});
+  
+
+  LatexDoc doc;
+  doc<<"\\section*{CG-product}\n";
+  
+  for(int _b=0; _b<b_val.size(); _b++){
+
+    doc<<"\\subsection*{Batch size "<<b_val[_b]<<"}\n";
+
+    TikzPlot plot(nc_val,flops0.slice(0,_b).slice(1,3).slice(1,3).transp());
+    doc<<plot.latex()<<"\\\\ \\\\ \nk";
+
+    for(int lix: vector<int>({1,3,5})){
+      if(lix>=l_val.size()) continue;
+      for(int ncix: vector<int>({1,3,5,7})){
+	if(ncix>=nc_val.size()) continue;
+
+	doc<<"\\subsubsection*{"<<"$\\ell$="<<l_val[lix]<<"~ $n_c$="<<nc_val[ncix]<<"}";
+	//doc<<"$\\ell$="<<l_val[lix]<<"\\\\ \n";
+	//doc<<"$n_c$="<<nc_val[ncix]<<"\\\\ \n";
+	LatexTable<int> table(l_val,l_val,flops0.slice(0,_b).slice(2,lix).slice(2,ncix));
+	doc<<table.latex()<<"\\\\ \n";
+	doc<<"\\\\ \\\\ \\\\ \n";
+      }
+    }
+  }
+
+  doc.compile("results");
 
 }
